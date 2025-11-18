685e00c076ddb0b2228ff1974b4b83a1c61102c8
Sergio Chica <Sergioch1994@gmail.com>
2025-11-18 13:39:02 +0100

    commit Ex5

--- Ejercicio4/src/main.js
+++ Ejercicio4/src/main.js
@@ -47,5 +47,22 @@ app.appendChild(buttonEx4);
 // Nota: Usar async-await
 // EXTRA: Por CORS, necesitareis poner:
 //  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-//   res.setHeader("Access-Control-Allow-Methods", "GET");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//  res.setHeader("Access-Control-Allow-Methods", "GET");
+//  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+// EJERCICIO 5:
+/* A,pliar ejercicio antrerior, con als siguiente omdificaciones:
+FRONT:
+Hacer peticiona una ruta http://localhost:3000/login, pasandole un email y una contraseña que se recoja de dos campos input de un formulario.
+Estos campos iran dentro del body a la hora de enviar la peticion fetch
+ 
+BACK:
+Tener un array de obejtos de usuario, con el formato: user = {name, email, pass, role} donde role puede xer o "admin" o "user"
+En la ruta para recibir la peticion del back, deberemos comporbar si existe un usuario que coincida
+su email passweord y role.
+De ser asi, devolvemos ese usuario y lo mostramos por consola
+Sino, dos opciones:
+Si coincide usuario y contraseña, devolver mensaje de no tiene privilegios
+Si no coinciden, devolver mensaje de email o contraseña invalidos.
+ 
+*/
